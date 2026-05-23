cd3b5b2a841fed03386bc3ec6b6f0aa8bd5ea4ec
oraby <amrorapy50@gmail.com>
2026-05-23 05:46:59 +0300

    Create Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import styles from "./Button.module.css";
 type ButtonProps = {
   children: React.ReactNode;
-  onClick: () => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   type: string;
 };
 function Button({ children, onClick, type }: ButtonProps) {
