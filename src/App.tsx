8677b24a96153a08171e03de484efeccdea3ac51
oraby <amrorapy50@gmail.com>
2026-05-24 10:11:21 +0300

    Configure React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { lazy, Suspense } from "react";
-import { CitiesProvider } from "./contexts/CitiesContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./contexts/FakeAuthContext";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import ProtectedRoute from "./pages/ProtectedRoute";
 import SpinnerFullPage from "./components/SpinnerFullPage";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Pricing = lazy(() => import("./pages/Pricing"));
 const Product = lazy(() => import("./pages/Product"));
@@ -18,38 +16,46 @@ const AppLayout = lazy(() => import("./pages/AppLayout"));
 const Login = lazy(() => import("./pages/Login"));
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // staleTime: 60 * 1000,
+        staleTime: 0,
+      },
+    },
+  });
+
   return (
-    <CitiesProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <Suspense fallback={<SpinnerFullPage />}>
-            <Routes>
-              <Route index element={<Homepage />} />
-              <Route path="*" element={<PageNotFound />} />
-              <Route path="product" element={<Product />} />
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="login" element={<Login />} />
-              <Route
-                path="app"
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                {/* before we was getting the citiylist but without the route now we have the url => /cities  */}
-                {/* before the navigater was a component in the history so we tell react replace it (remove it from history) */}
-                <Route index element={<Navigate replace to="cities" />} />
-                <Route path="cities" element={<CityList />} />a
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
-              </Route>
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-      </AuthProvider>
-    </CitiesProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route
+              path="app"
+              element={
+                // <ProtectedRoute>
+                <AppLayout />
+                // </ProtectedRoute>
+              }
+            >
+              {/* before we was getting the citiylist but without the route now we have the url => /cities  */}
+              {/* before the navigater was a component in the history so we tell react replace it (remove it from history) */}
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />a
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
