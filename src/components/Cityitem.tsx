eb217e7ebdee5e01a8259b249b1f0588509280f0
oraby <amrorapy50@gmail.com>
2026-05-24 11:40:45 +0300

    Create CityItem

--- src/components/Cityitem.tsx
+++ src/components/Cityitem.tsx
@@ -1,42 +1,53 @@
-import { useCities } from '../contexts/CitiesContext';
-import styles from './Cityitem.module.css'
-import { Link} from 'react-router-dom';
+import useCreateCity from "../features/cities/useCreateCity";
+import useDeleteCity from "../features/cities/useDeleteCity";
+import type { CityType } from "../types/cityTypes";
+import styles from "./Cityitem.module.css";
+import { Link } from "react-router-dom";
 
-const formatDate = (date) =>
-    new Intl.DateTimeFormat("en", {
+const formatDate = (date: string) =>
+  new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "long",
     year: "numeric",
     weekday: "long",
-}).format(new Date(date));
+  }).format(new Date(date));
 
-const flagemojiToPNG = (flag) => {
-    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt()).map(char => String.fromCharCode(char-127397).toLowerCase()).join('')
-    return (<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />)
+const flagemojiToPNG = (flag: string) => {
+  const countryCode = Array.from(flag, (codeUnit: string) =>
+    codeUnit.codePointAt(0),
+  )
+    .map((char) => String.fromCharCode((char as number) - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
+function Cityitem({ city }: { city: CityType }) {
+  const { cityName, emoji, date, id, lat, lng } = city;
+  const { deleteCity } = useDeleteCity();
+  const { currentCity } = useCreateCity();
+
+  function handleDelete(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${id === currentCity?.id ? styles[`cityItem--active`] : ""}`}
+        to={`${id}?lat=${lat}&lng=${lng}`}
+      >
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName} </h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn} onClick={handleDelete}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
 }
 
-function Cityitem({city}) {
-    const {cityName,emoji,date,id,position} = city  
-    const {currentCity,deleteCity} = useCities();
-    
-    function handleDelete(e) {
-        e.preventDefault();
-        deleteCity(id)
-
-    }
-    return (
-        <li>
-            <Link className={`${styles.cityItem} ${id === currentCity.id ? styles[`cityItem--active`] : ""}`}
-            to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
-                <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>         
-                <h3 className={styles.name}>{cityName}  </h3>
-                <time className={styles.date}>{formatDate(date)}</time>   
-                <button className={styles.deleteBtn} onClick={handleDelete  }>&times;</button>      
-            </Link>
-        </li>
-    )
-}
-
-export default Cityitem
-
-
+export default Cityitem;
