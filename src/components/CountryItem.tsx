7c7bc4a4a73bbe3925eea8a845b4f851b3ef39b0
oraby <amrorapy50@gmail.com>
2026-05-24 11:07:54 +0300

    Create CountryItem

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -1,6 +1,7 @@
+import type { CountryType } from "../types/countryTypes";
 import styles from "./CountryItem.module.css";
 
-function CountryItem({ country }) {
+function CountryItem({ country }: { country: CountryType }) {
   return (
     <li className={styles.countryItem}>
       <span>{country.emoji}</span>
