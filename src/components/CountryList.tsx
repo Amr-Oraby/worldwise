ba3fe90523fb0ab685de194eba8145dbe82806a0
oraby <amrorapy50@gmail.com>
2026-05-24 11:07:36 +0300

    Create CountryList

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,31 +1,38 @@
-import styles from './CountryList.module.css'
-import Spinner from "../components/Spinner"
-import Message from "./Message"
-import CountryItem from './CountryItem';
-import { useCities } from '../contexts/CitiesContext';
+import useCities from "../features/cities/useCities";
+import type { CityType } from "../types/cityTypes";
+import type { CountryType } from "../types/countryTypes";
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
 function CountryList() {
-        const {isLoading,cities} = useCities()
+  const { isLoading, cities } = useCities();
 
-    if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
 
-    if(!cities.length) return <Message message="Add your first Country by clicking on a country on the map" />
-
-    // logic code for preventing repeating countries 
-    const countries = cities.reduce((arr,city)=>{
-        if(!arr.map(el=>el.country).includes(city.country)){ 
-            return [...arr,{country:city.country,emoji:city.emoji,id:city.id}]
-        } else return arr
-    },[]);
-
+  if (!cities?.length)
     return (
-        <ul className={styles.countryList}>
-            {
-                countries.map(country => (
-                <CountryItem key={country.id} country={country} />
-                ))   
-            }
-        </ul>
-    )
+      <Message message="Add your first Country by clicking on a country on the map" />
+    );
+
+  // logic code for preventing repeating countries
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el: CityType) => el.country).includes(city.country)) {
+      return [
+        ...arr,
+        { country: city.country, emoji: city.emoji, id: city.id },
+      ];
+    } else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country: CountryType) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
 }
 
-export default CountryList
+export default CountryList;
