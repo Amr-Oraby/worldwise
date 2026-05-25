8a999dcfaf28d3fc2dccd194e7fc9f0eacaf7263
oraby <amrorapy50@gmail.com>
2026-05-25 06:57:45 +0300

    Update useCreateCity

--- src/features/cities/useCreateCity.ts
+++ src/features/cities/useCreateCity.ts
@@ -1,15 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCity as createCityApi } from "../../services/apiCities";
 import { useState } from "react";
 import type { CityType } from "../../types/cityTypes";
 
 function useCreateCity() {
+  const queryClient = useQueryClient();
   const [currentCity, setCurrentCity] = useState<CityType | null>(null);
-  const { mutate: createCity, isPending } = useMutation({
+  const { mutate: createCity, isPending: isCreating } = useMutation({
     mutationFn: createCityApi,
-    onSuccess: (data: CityType) => setCurrentCity(data),
+    onSuccess: (data: CityType) => {
+      setCurrentCity(data);
+      queryClient.invalidateQueries({ queryKey: ["cities"] });
+    },
   });
-  return { createCity, isPending, currentCity };
+  return { createCity, isCreating, currentCity };
 }
 
 export default useCreateCity;
