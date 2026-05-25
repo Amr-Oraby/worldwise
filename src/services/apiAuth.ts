b73c39b9de2cb76087cb0e46c944a088ed7a1935
oraby <amrorapy50@gmail.com>
2026-05-25 08:18:14 +0300

    Update apiAuth

---
+++ src/services/apiAuth.ts
@@ -0,0 +1,36 @@
+import { supabase } from "./supbase";
+
+export async function login({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    console.error(error);
+    throw new Error("There was an error logining in");
+  }
+  return data;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error(error);
+    throw new Error("There was an error logining out");
+  }
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
