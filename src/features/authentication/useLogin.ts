31dc5408859c10403ab70e9a2d58f60baa5351e9
oraby <amrorapy50@gmail.com>
2026-05-29 21:20:52 +0900

    Update useLogin

--- src/features/authentication/useLogin.ts
+++ src/features/authentication/useLogin.ts
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isPending: isLoginingIn } = useMutation({
     mutationFn: loginApi,
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
       navigate("/app");
     },
   });
