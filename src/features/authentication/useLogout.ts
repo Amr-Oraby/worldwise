0ad5c46e61471ccb44a09106d96ea9b697d9c421
oraby <amrorapy50@gmail.com>
2026-05-29 22:24:14 +0900

    Update useLogout

--- src/features/authentication/useLogout.ts
+++ src/features/authentication/useLogout.ts
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 function useLogout() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: logout, isPending: isLoginingOut } = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
       navigate("/");
     },
   });
